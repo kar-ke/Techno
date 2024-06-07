d3184d07a2a4a33c26d7e96b3f747416d85d192c
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-06-07 21:56:50 +0530

    about section

--- Script/script.js
+++ Script/script.js
@@ -1,21 +1,53 @@
 
-const swiper = new Swiper('.swiper', {
+// navbar actions 
+$(document).ready(function () {
+    
+    $('.open-menu').click(function () {
+        $('.mobile-navbar-menulists').addClass('toogleopen');
+        $('.open-menu').addClass('hidden')
+        $('.close-menu').removeClass('hidden')
+    })
+    $('.close-menu').click(function () {
+        $('.mobile-navbar-menulists').removeClass('toogleopen');
+        $('.close-menu').addClass('hidden')
+        $('.open-menu').removeClass('hidden')
+    })
 
-    // autoplay: {
-    //     delay: 3000,
-    // },
+
+    // submenu 
+    $('.primarynav-item').hover(function () {
+        $(this).find('.sub-menu').css('visibility', 'visible');
+    }, function () {
+        $(this).find('.sub-menu').css('visibility', 'hidden');
+        
+    })
+
+})
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 3000
+    },
     speed: 1000,
     loop: true,
-    
+
+    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-        el: '.swiper-scrollbar',
+        clickable: true
     },
 
+    // Navigation arrows
+    navigation: {
+        enabled: false,
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+    el: '.swiper-scrollbar',
+    },
 });
