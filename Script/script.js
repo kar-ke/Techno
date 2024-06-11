6a6a89c13192a241e9016896974ed7d3c453c3ab
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-06-11 17:22:55 +0530

    services slider added

--- Script/script.js
+++ Script/script.js
@@ -2,6 +2,28 @@
 // navbar actions 
 $(document).ready(function () {
     
+
+    $("#service-cards-slider").owlCarousel({
+        margin: 30,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576:{
+                items:2,
+            },
+            768:{
+                items:2,
+            },
+            992:{
+                items:3,
+            }
+        }
+    });
+
+
+
     $('.open-menu').click(function () {
         $('.mobile-navbar-menulists').addClass('toogleopen');
         $('.open-menu').addClass('hidden')
@@ -26,8 +48,8 @@ $(document).ready(function () {
 
 
 
-
-
+// Swiper js properties 
+// Hero page carousel 
 const swiper = new Swiper('.swiper', {
     autoplay: {
         delay: 3000
@@ -35,19 +57,27 @@ const swiper = new Swiper('.swiper', {
     speed: 1000,
     loop: true,
 
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true
     },
 
-    // Navigation arrows
     navigation: {
         enabled: false,
     },
 
-    // And if we need scrollbar
     scrollbar: {
     el: '.swiper-scrollbar',
+    },
+});
+
+
+// service cards swiper 
+const service_swiper = new Service_swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
 });
